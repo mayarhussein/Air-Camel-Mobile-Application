848041ca58380e8b35612ddd445045a4be8dba9a
Mayar Hussein <mayar_memoo@ymail.com>
2022-04-24 21:12:14 +0200

    commit

--- lib/widgets/address_bottom_sheet.dart
+++ lib/widgets/address_bottom_sheet.dart
@@ -124,177 +124,179 @@ class AddressBottomSheet {
             padding: MediaQuery.of(context).viewInsets,
             child: Container(
                 padding: mainPadding,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Center(
-                      child: Text(
-                        isEdit ? 'Edit Address' : "New Address",
-                        style: headFont1,
+                child: SingleChildScrollView(
+                  reverse: true,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Center(
+                        child: Text(
+                          isEdit ? 'Edit Address' : "New Address",
+                          style: headFont1,
+                        ),
                       ),
-                    ),
-                    SingleChildScrollView(
-                      child: Form(
-                          key: _formKey,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              TextFormField(
-                                key: const ValueKey('City'),
-                                decoration:
-                                    const InputDecoration(labelText: "City *"),
-                                focusNode: _cityFocusNode,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter city';
-                                  }
-                                  return null;
-                                },
-                                controller: cityController,
-                                onFieldSubmitted: (_) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_streetFocusNode);
-                                },
-                              ),
-                              TextFormField(
-                                key: const ValueKey('Street'),
-                                decoration: const InputDecoration(
-                                    labelText: "Street *"),
-                                focusNode: _streetFocusNode,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter street';
-                                  }
-                                  return null;
-                                },
-                                controller: streetController,
-                                onFieldSubmitted: (_) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_buildingFocusNode);
-                                },
-                              ),
-                              TextFormField(
-                                key: const ValueKey('Building'),
-                                decoration: const InputDecoration(
-                                    labelText: "Building *"),
-                                focusNode: _buildingFocusNode,
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter building';
-                                  }
-                                  return null;
-                                },
-                                controller: buildingController,
-                                onFieldSubmitted: (_) {
-                                  FocusScope.of(context)
-                                      .requestFocus(_floorFocusNode);
-                                },
-                              ),
-                              Row(children: <Widget>[
-                                SizedBox(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.45,
+                      Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                TextFormField(
+                                  key: const ValueKey('City'),
+                                  decoration:
+                                      const InputDecoration(labelText: "City *"),
+                                  focusNode: _cityFocusNode,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter city';
+                                    }
+                                    return null;
+                                  },
+                                  controller: cityController,
+                                  onFieldSubmitted: (_) {
+                                    FocusScope.of(context)
+                                        .requestFocus(_streetFocusNode);
+                                  },
+                                ),
+                                TextFormField(
+                                  key: const ValueKey('Street'),
+                                  decoration: const InputDecoration(
+                                      labelText: "Street *"),
+                                  focusNode: _streetFocusNode,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter street';
+                                    }
+                                    return null;
+                                  },
+                                  controller: streetController,
+                                  onFieldSubmitted: (_) {
+                                    FocusScope.of(context)
+                                        .requestFocus(_buildingFocusNode);
+                                  },
+                                ),
+                                TextFormField(
+                                  key: const ValueKey('Building'),
+                                  decoration: const InputDecoration(
+                                      labelText: "Building *"),
+                                  focusNode: _buildingFocusNode,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter building';
+                                    }
+                                    return null;
+                                  },
+                                  controller: buildingController,
+                                  onFieldSubmitted: (_) {
+                                    FocusScope.of(context)
+                                        .requestFocus(_floorFocusNode);
+                                  },
+                                ),
+                                Row(children: <Widget>[
+                                  SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.45,
+                                      child: TextFormField(
+                                        key: const ValueKey('Floor'),
+                                        decoration: const InputDecoration(
+                                            labelText: "Floor"),
+                                        focusNode: _floorFocusNode,
+                                        controller: floorController,
+                                        onFieldSubmitted: (_) {
+                                          FocusScope.of(context)
+                                              .requestFocus(_aptFocusNode);
+                                        },
+                                      )),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.45,
                                     child: TextFormField(
-                                      key: const ValueKey('Floor'),
+                                      key: const ValueKey('Apartment'),
                                       decoration: const InputDecoration(
-                                          labelText: "Floor"),
-                                      focusNode: _floorFocusNode,
-                                      controller: floorController,
+                                          labelText: "Apartment"),
+                                      focusNode: _aptFocusNode,
+                                      controller: aptController,
                                       onFieldSubmitted: (_) {
                                         FocusScope.of(context)
-                                            .requestFocus(_aptFocusNode);
+                                            .requestFocus(_otherFocusNode);
                                       },
-                                    )),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.45,
-                                  child: TextFormField(
-                                    key: const ValueKey('Apartment'),
-                                    decoration: const InputDecoration(
-                                        labelText: "Apartment"),
-                                    focusNode: _aptFocusNode,
-                                    controller: aptController,
-                                    onFieldSubmitted: (_) {
-                                      FocusScope.of(context)
-                                          .requestFocus(_otherFocusNode);
-                                    },
-                                  ),
+                                    ),
+                                  )
+                                ]),
+                                TextFormField(
+                                  keyboardType: TextInputType.multiline,
+                                  minLines: 1,
+                                  key: const ValueKey('Other'),
+                                  decoration: const InputDecoration(
+                                      labelText:
+                                          "Add any additional description"),
+                                  focusNode: _otherFocusNode,
+                                  controller: otherController,
+                                  onFieldSubmitted: (_) {
+                                    if (_formKey.currentState!.validate()) {
+                                      _AddAddress(
+                                          cityController.text.trim(),
+                                          streetController.text.trim(),
+                                          buildingController.text.trim(),
+                                          floorController.text.trim(),
+                                          aptController.text.trim(),
+                                          otherController.text.trim());
+                                    }
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Fields marked as * are REQUIRED",
+                                    style: hintFont),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Center(
+                                  child: ElevatedButton(
+                                      child: Text(
+                                          isEdit ? 'Edit Address' : 'Add Address',
+                                          style: const TextStyle(
+                                              color: Colors.white)),
+                                      onPressed: () {
+                                        if (isEdit) {
+                                          print('Edit will be done here');
+                                          if (_formKey.currentState!.validate()) {
+                                            _editAddress(
+                                                cityController.text.trim(),
+                                                streetController.text.trim(),
+                                                buildingController.text.trim(),
+                                                floorController.text.trim(),
+                                                aptController.text.trim(),
+                                                otherController.text.trim());
+                                          }
+                                        } else {
+                                          if (_formKey.currentState!.validate()) {
+                                            _AddAddress(
+                                                cityController.text.trim(),
+                                                streetController.text.trim(),
+                                                buildingController.text.trim(),
+                                                floorController.text.trim(),
+                                                aptController.text.trim(),
+                                                otherController.text.trim());
+                                          }
+                                        }
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                          side: const BorderSide(
+                                              width: 3, color: Colors.amber),
+                                          elevation: 3,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(50)),
+                                          padding: const EdgeInsets.all(
+                                              20) //content padding inside button
+                                          )),
                                 )
-                              ]),
-                              TextFormField(
-                                keyboardType: TextInputType.multiline,
-                                minLines: 1,
-                                key: const ValueKey('Other'),
-                                decoration: const InputDecoration(
-                                    labelText:
-                                        "Add any additional description"),
-                                focusNode: _otherFocusNode,
-                                controller: otherController,
-                                onFieldSubmitted: (_) {
-                                  if (_formKey.currentState!.validate()) {
-                                    _AddAddress(
-                                        cityController.text.trim(),
-                                        streetController.text.trim(),
-                                        buildingController.text.trim(),
-                                        floorController.text.trim(),
-                                        aptController.text.trim(),
-                                        otherController.text.trim());
-                                  }
-                                },
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text("Fields marked as * are REQUIRED",
-                                  style: hintFont),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Center(
-                                child: ElevatedButton(
-                                    child: Text(
-                                        isEdit ? 'Edit Address' : 'Add Address',
-                                        style: const TextStyle(
-                                            color: Colors.white)),
-                                    onPressed: () {
-                                      if (isEdit) {
-                                        print('Edit will be done here');
-                                        if (_formKey.currentState!.validate()) {
-                                          _editAddress(
-                                              cityController.text.trim(),
-                                              streetController.text.trim(),
-                                              buildingController.text.trim(),
-                                              floorController.text.trim(),
-                                              aptController.text.trim(),
-                                              otherController.text.trim());
-                                        }
-                                      } else {
-                                        if (_formKey.currentState!.validate()) {
-                                          _AddAddress(
-                                              cityController.text.trim(),
-                                              streetController.text.trim(),
-                                              buildingController.text.trim(),
-                                              floorController.text.trim(),
-                                              aptController.text.trim(),
-                                              otherController.text.trim());
-                                        }
-                                      }
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                        side: const BorderSide(
-                                            width: 3, color: Colors.amber),
-                                        elevation: 3,
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(50)),
-                                        padding: const EdgeInsets.all(
-                                            20) //content padding inside button
-                                        )),
-                              )
-                            ],
-                          )),
-                    ),
-                  ],
+                              ],
+                            )),
+                      
+                    ],
+                  ),
                 )),
           );
         });
